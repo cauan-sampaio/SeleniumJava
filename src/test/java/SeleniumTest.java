c9cafcca5b9726b0ef0faa4eef247d6494934b94
Francisco Cauan Almeida Sampaio <francisco.cauan.almeida61@aluno.ifce.edu.br>
2024-07-09 13:47:42 -0300

    Selenium testes

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -1,13 +1,33 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.devtools.Message;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
+
 public class SeleniumTest {
-    @Test
-    void Setup(){
-        System.setProperty("webdriver.gecko.driver",  "/usr/local/bin/");
-        WebDriver webDriver = new FirefoxDriver();
-        webDriver.get("https://www.youtube.com/");
+    @BeforeTest
+    public void Setup() throws InterruptedException{
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") +  "/src/test/resources/chromedriver.exe");
+        WebDriver webDriver = new ChromeDriver(options);
+        webDriver.get("https://www.selenium.dev/selenium/web/web-form.html");
+        String title = webDriver.getTitle();
+        System.out.println(title);
+        WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(10));
+
+        WebElement textBox = webDriver.findElement(By.name("my-text"));
+       
+        WebElement submitButton = webDriver.findElement(By.cssSelector("button"));
+        textBox.sendKeys("Selenium");
+        Thread.sleep(5000);
+        submitButton.click();
+        //webDriver.quit();
     }
 }
